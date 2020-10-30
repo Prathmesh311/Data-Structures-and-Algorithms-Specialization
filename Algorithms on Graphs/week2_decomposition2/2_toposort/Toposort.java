19a910df0c1a22b776cfd8ba39ed746c89104690
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2020-10-30 16:32:22 +0530

    toposort

--- Algorithms on Graphs/week2_decomposition2/2_toposort/Toposort.java
+++ Algorithms on Graphs/week2_decomposition2/2_toposort/Toposort.java
@@ -1,17 +1,37 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class Toposort {
     private static ArrayList<Integer> toposort(ArrayList<Integer>[] adj) {
+
         int used[] = new int[adj.length];
-        ArrayList<Integer> order = new ArrayList<Integer>();
-        //write your code here
-        return order;
+        ArrayList<Integer> order = new ArrayList<Integer>();        // arraylist for storing order of node visit
+        Arrays.fill(used, 0);
+
+        for(int i=0; i < adj.length; i++)                           // DFS
+        {
+            if(used[i] == 0)
+            {
+                dfs(adj, used, order, i);
+            }
+        }
+        return order;                                               // return order
     }
 
     private static void dfs(ArrayList<Integer>[] adj, int[] used, ArrayList<Integer> order, int s) {
       //write your code here
+        used[s] = 1;                            // make node as visited
+
+        for (int i: adj[s])                    // iterate through adjacent nodes
+        {
+            if(used[i] == 0)
+            {
+                dfs(adj, used, order, i);      // if not visited then explore
+            }
+        }
+        order.add(0, s);                 // add element in order to keep track of order of visiting of node in TopoSort
     }
 
     public static void main(String[] args) {
